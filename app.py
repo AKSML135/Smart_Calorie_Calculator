6102cc5310da5193b030f9dd4c0d074adbe6aa45
AMAN KUMAR SRIVASTAVA <51173566+AKSML135@users.noreply.github.com>
2024-02-09 18:26:22 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,135 @@
+# import streamlit as st
+# from dotenv import load_dotenv
+# import google.generativeai
+# from PIL import Image
+# import os
+
+# load_dotenv()
+# #configuring gemini with our api key
+# google.generativeai.configure(api_key = os.getenv('GOOGLE_API_KEY'))
+
+# def get_gemini_response(prompt, image):
+#     model = google.generativeai.GenerativeModel('gemini-pro-vision')
+#     response = model.generate_content([image[0],prompt])
+
+# def input_image_setup(uploaded_file):
+#     # Check if a file has been uploaded
+#     if uploaded_file is not None:
+#         # Read the file into bytes
+#         bytes_data = uploaded_file.getvalue()
+
+#         image_parts = [
+#             {
+#                 "mime_type": uploaded_file.type,  # Get the mime type of the uploaded file
+#                 "data": bytes_data
+#             }
+#         ]
+#         return image_parts
+#     else:
+#         raise FileNotFoundError("No file uploaded")
+    
+
+# #initialising our streamlit app
+# st.set_page_config(page_title = 'Gemini Health App')
+# #Defining title and other components
+# st.title('Smart Calorie Count App 🍕🥦')
+# input = st.text_input("Input Prompt: ",key="input")
+# uploaded_file = st.file_uploader('Choose an image of your food..🧑🏽‍🍳', type= ["jpg","jpeg","png"])
+# image = ""
+# if uploaded_file is not None:
+#     image = Image.open(uploaded_file)
+#     #Displaying uploaded image 
+#     st.image(image,caption="Image Uploaded Thanks..", use_column_width=True)
+
+# submit = st.button("Click Me to know the calories ☞")
+# input_prompt = """
+# You are an expert in nutritionist where you need to see the food items from the image
+#                and calculate the total calories, also provide the details of every food items with calories intake
+#                is below format
+
+#                1. Item 1 - no of calories
+#                2. Item 2 - no of calories
+#                ----
+#                ----
+
+
+# """
+
+# ## If submit button is clicked
+
+# if submit:
+#     image_data = input_image_setup(uploaded_file)
+#     response = get_gemini_response(input_prompt,image_data)
+#     print(response)
+#     st.subheader('The Response is')
+#     st.write(response)
+### Health Management APP
+from dotenv import load_dotenv
+
+load_dotenv() ## load all the environment variables
+
+import streamlit as st
+import os
+import google.generativeai as genai
+from PIL import Image
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+## Function to load Google Gemini Pro Vision API And get response
+
+def get_gemini_repsonse(input,image,prompt):
+    model=genai.GenerativeModel('gemini-pro-vision')
+    response=model.generate_content([input,image[0],prompt])
+    return response.text
+
+def input_image_setup(uploaded_file):
+    # Check if a file has been uploaded
+    if uploaded_file is not None:
+        # Read the file into bytes
+        bytes_data = uploaded_file.getvalue()
+
+        image_parts = [
+            {
+                "mime_type": uploaded_file.type,  # Get the mime type of the uploaded file
+                "data": bytes_data
+            }
+        ]
+        return image_parts
+    else:
+        raise FileNotFoundError("No file uploaded")
+    
+##initialize our streamlit app
+
+st.set_page_config(page_title="Gemini Health App")
+
+st.title('Smart Calorie Count App 🍕🥦')
+input=st.text_input("Input Prompt: ",key="input")
+uploaded_file = st.file_uploader("Choose an image of your food..🧑🏽‍🍳", type=["jpg", "jpeg", "png"])
+image=""   
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Image Uploaded Thanks..", use_column_width=True)
+
+
+submit=st.button("Click Me to know the calories ☞")
+
+input_prompt="""
+You are an expert in nutritionist where you need to see the food items from the image
+               and calculate the total calories, also provide the details of every food items with calories intake
+               is below format
+
+               1. Item 1 - no of calories
+               2. Item 2 - no of calories
+               ----
+               ----
+
+
+"""
+
+## If submit button is clicked
+
+if submit:
+    image_data=input_image_setup(uploaded_file)
+    response=get_gemini_repsonse(input_prompt,image_data,input)
+    st.subheader("The Response is..")
+    st.write(response)
